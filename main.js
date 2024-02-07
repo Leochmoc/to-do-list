c1cb28dfbfc52da138db736fd24c8044567e5bff
leochmoc <leochmoc@gmail.com>
2024-02-06 22:34:54 -0500

    Se soluciona || toogle x toggle

--- main.js
+++ main.js
@@ -18,10 +18,10 @@ function addTask() {
 
 listContainer.addEventListener("click", function(e){
   if(e.target.tagName === "LI"){
-      e.target.classList.toogle("checked");
+      e.target.classList.toggle("checked");
       saveData()
   } else if(e.target.tagName === "SPAN"){
-    e.target.parentElement.remove();
+      e.target.parentElement.remove();
       saveData()
   }
 }, false);
